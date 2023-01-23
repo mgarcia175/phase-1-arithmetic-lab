6aca5c9d714029eac80c8a13837b2401ef004bb1
Martin <martingarcia804@yahoo.com>
2023-01-22 20:14:25 -0600

    Completed Lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1 = 31
+const num2 = 2
+let multiply
+multiply = num1 * num2
+
+let random
+random = Math.floor(Math.random() * 10) + 1;
+
+let mod
+let num3 = 3
+let num4 = 1
+mod = num3 + num4
+
+let max
+max = Math.max(5, 10, 15, 20);
